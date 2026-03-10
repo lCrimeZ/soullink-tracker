5727bbec09f815f85139529d7a015f38e6fb164e
lCrimeZ <s.gerve@yahoo.de>
2026-03-10 14:44:32 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,9 @@ export default function RootLayout({
           <div className="absolute inset-0 opacity-[0.04] mix-blend-overlay pointer-events-none [background-image:url('data:image/svg+xml;utf8,<svg xmlns=%22http://www.w3.org/2000/svg%22 width=%22228%22 height=%22228%22 viewBox=%220 0 228 228%22><filter id=%22n%22><feTurbulence type=%22fractalNoise%22 baseFrequency=%220.8%22 numOctaves=%222%22 stitchTiles=%22stitch%22/></filter><rect width=%22228%22 height=%22228%22 filter=%22url(%23n)%22 opacity=%220.18%22/></svg>')]" />
         </div>
 
-        <div className="relative min-h-screen max-w-[1200px] mx-auto px-6">
+        <div className="relative min-h-screen max-w-[1300px] mx-auto px-6">
+          {children}
+        </div>
       </body>
     </html>
   );
