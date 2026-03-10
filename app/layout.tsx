942f8a38d75a53914e42c05c4a2be6568018151e
lCrimeZ <s.gerve@yahoo.de>
2026-03-10 15:18:11 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,11 +62,13 @@ export default function RootLayout({
           <div className="absolute inset-0 opacity-[0.05] mix-blend-overlay pointer-events-none [background-image:url('data:image/svg+xml;utf8,<svg xmlns=%22http://www.w3.org/2000/svg%22 width=%22228%22 height=%22228%22 viewBox=%220 0 228 228%22><filter id=%22n%22><feTurbulence type=%22fractalNoise%22 baseFrequency=%220.8%22 numOctaves=%222%22 stitchTiles=%22stitch%22/></filter><rect width=%22228%22 height=%22228%22 filter=%22url(%23n)%22 opacity=%220.18%22/></svg>')]" />
         </div>
 
-        {/* Content */}
+          {/* Content */}
         <div className="relative min-h-screen">
           <div className="hooh-aura" />
+
           <div className="content-stage">
-          {children}
+            {children}
+          </div>
         </div>
       </body>
     </html>
